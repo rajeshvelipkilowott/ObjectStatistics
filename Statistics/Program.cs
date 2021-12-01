1df416d2aee801345a49625fe4fa493acd56660e
rajeshvelipkilowott <rajesh.velip@kilowott.com>
2021-12-01 16:25:45 +0530

    Add project files.

---
+++ Statistics/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    internal class Program
+    {
+        static int[] data = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+
+        static void Main(String[] args)
+        {
+            var m1 = new StatsProcessor();
+            m1.Numbers = data;
+            var mn = m1.Mean();
+            Console.WriteLine($"Mean: {mn}");
+            var md = m1.Median();
+            Console.WriteLine($"Median: {md}");
+            var mo = m1.Mode();
+            Console.WriteLine($"Mode: {mo}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Statistics/StatsProcessor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    internal class StatsProcessor
+    {
+       
+        internal int[] Numbers { private get; set; }
+        
+        
+        internal decimal Mean()
+        {
+            int sum = 0;
+            decimal mean = 0;
+            {
+                foreach (int test in Numbers)
+                {
+                    sum = sum + test;
+                }
+                mean =(decimal) sum / Numbers.Length;
+                return mean;
+            }
+        }
+
+        internal int Median()
+        {
+            int x = (Numbers.Length) / 2;
+            int y = Numbers[x];
+            return y;
+        }
+
+        internal int Mode()
+        {
+             int maxcount = 0, maxnum = 0;
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                int count = 0;
+                for (int j = 1; j < Numbers.Length - 1; j++)
+                {
+                    if (Numbers[i] == Numbers[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > maxcount)
+                {
+                    maxcount = count;
+                    maxnum = Numbers[i];
+                }
+            }
+            return maxnum;
+        }
+        
+    }
+}
