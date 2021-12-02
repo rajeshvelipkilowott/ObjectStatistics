43f2001746c6c7fa5bd218c463dea4336419fd72
rajeshvelipkilowott <92842921+rajeshvelipkilowott@users.noreply.github.com>
2021-12-02 18:10:37 +0530

    Add files via upload

---
+++ NewResult/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewResult
+{
+    internal class Program
+    {
+        static int[] data = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19, 42 };
+
+        static void Main(string[] args)
+        {
+            var spObject = new StatsProcessor();
+
+
+            spObject.Numbers = data;
+
+            var result = spObject.GetStatsProcessorResult();
+
+
+            Console.WriteLine($"Average: {result.Mean}");
+            Console.WriteLine($"Median: {result.Median}");
+            Console.WriteLine($"Mode: {result.Mode}");
+        }
+    }
+}
---
+++ NewResult/StatsProcessor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewResult
+{
+    internal class StatsProcessor
+    {
+        internal int[] Numbers { private get; set; }
+
+        internal StatsResult GetStatsProcessorResult()
+        {
+            var result = new StatsResult();
+            result.Mean = CalculateAverage();
+            result.Median = CalculateMedian();
+            result.Mode = CalculateMode();
+            return result;
+        }
+
+        private int CalculateAverage() => Numbers.Sum() / Numbers.Length;
+
+        private int CalculateMedian()
+        {
+            var sortedData = Numbers.OrderBy(i => i).ToArray();
+            var n = sortedData.Length;
+            var median = n % 2 == 0
+                ? (sortedData[n / 2 - 1] + sortedData[n / 2]) / 2
+                : sortedData[n / 2];
+            return median;
+        }
+        private int CalculateMode()
+        {
+            int maxcount = 0, maxnum = 0;
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                int count = 0;
+                for (int j = 1; j < Numbers.Length - 1; j++)
+                {
+                    if (Numbers[i] == Numbers[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > maxcount)
+                {
+                    maxcount = count;
+                    maxnum = Numbers[i];
+                }
+            }
+            return maxnum;
+        }
+
+    }
+}
